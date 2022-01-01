fbbb5526f9e105930a32184e647d08bcb1d536b3
haithem <haithem2001@gmail.com>
2022-01-01 16:23:31 +0100

    M

--- pages/submissions/calculate-area-of-circle-without-pi/index.tsx
+++ pages/submissions/calculate-area-of-circle-without-pi/index.tsx
@@ -1,33 +1,25 @@
 import { useRef, useState } from "react"
 import { roundArray, roundMatrix, roundNum, Show1DMatrix, Show2DMatrix } from "../../../components/util"
-import { jacobMethodIterations, jacobMethodPrecision, verifySolvable } from "./script"
+import { CalculateAreaOfCircle } from "./script"
 
 
 function Jacob() {
     const [runTime, setRunTime] = useState(0)
-    const [IterationsOrPrecision, setIterationsOrPrecision] = useState(100)
-    const [oldResults, setoldResults] = useState<number[][] | null>([])
-    const [result, setResult] = useState<number[] | null>(null)
-
-    const [workType, setWorkType] = useState("Iterations")
+    const [oldResults, setoldResults] = useState<number[] | null>([])
+    const [result, setResult] = useState<number | null>(null)
 
     const resultRef = useRef(null)
 
-    const defaultA = [
-        [10, -1, 2, 0],
-        [-1, 11, -1, 3],
-        [2, -1, 10, -1],
-        [0, 3, -1, 8]
-    ]
+    const defaultS = 1;
+    const defaultN = 4;
 
-    const defaultB = [6, 25, -11, 15];
+    const [s, setS] = useState(defaultS)
+    const [n, setN] = useState(defaultN)
 
-    const [A, setA] = useState<number[][]>(defaultA);
-    const [B, setB] = useState<number[]>(defaultB);
 
     const [errA, setErrA] = useState(null);
     const [errB, setErrB] = useState(null);
-    const [errG, setErrorG] = useState(null);
+    const [errG, setErrorG] = useState<any>(null);
 
     const solve = (e: any) => {
         e.preventDefault()
@@ -40,21 +32,18 @@ function Jacob() {
 
         setErrorG(null)
 
-
-        verifySolvable(A).then(async () => {
-            (workType == "Iterations" ?
-                jacobMethodIterations(A, B, IterationsOrPrecision) :
-                jacobMethodPrecision(A, B, IterationsOrPrecision)
-            ).then(({ x, allResults }) => {
+        CalculateAreaOfCircle(s, n).then(({ result, allResults }) => {
+            try {
                 setRunTime(performance.now() - TimeA)
-                setResult(roundArray(x, workType == "Iterations" ? 1e-2 : +IterationsOrPrecision));
-                setoldResults(roundMatrix(allResults, 1e-4))
+                setResult(result);
+                setoldResults(allResults)
                 // @ts-ignore
                 resultRef.current.scrollIntoView({ behavior: "smooth" })
-            })
-        }).catch((e: any) => {
-            setErrorG(e)
+            } catch (e) {
+                setErrorG(result + ": " + e)
+            }
         })
+
     }
 
 
@@ -75,18 +64,16 @@ function Jacob() {
 
     return <div className="md:p-20 lg:p-52">
         <div className="p-3 rounded-lg bg-gray-800">
-            <h1 className="text-2xl px-2 md:m-5">Jacob Method</h1>
-            the Jacobi method is an iterative algorithm for determining the solutions of a strictly diagonally dominant system of linear equations. Each diagonal element is solved for, and an approximate value is plugged in. The process is then iterated until it converges. This algorithm is a stripped-down version of the Jacobi transformation method of matrix diagonalization. The method is named after Carl Gustav Jacob Jacobi.
-            <h1 className="text-2xl px-2 m-5">A*X=B</h1>
+            <h1 className="text-2xl px-2 md:m-5">Calculate Area Of a circule</h1>
         </div>
 
         <form className="flex flex-col justify-center items-center">
-            <div className="w-full p-5 border-b-2 ">
-
+            <div className="w-full p-5  ">
+                S:<input type="number" value={s} onChange={(e) => setS(+e.target.value)} className="appearance-none  bg-gray-600 text-gray-300 border border-gray-600 rounded  mb-3 leading-tight focus:outline-none focus:bg-gray-700 focus:border-gray-500" />
                 {errA && <h4 className="text-white bg-red-600 rounded-lg">{errA}</h4>}
             </div>
             <div className="w-full p-5">
-
+                N:<input type="number" value={n} onChange={(e) => setN(+e.target.value)} className="appearance-none  bg-gray-600 text-gray-300 border border-gray-600 rounded  mb-3 leading-tight focus:outline-none focus:bg-gray-700 focus:border-gray-500" />
                 {errB && <h4 className="text-white bg-red-600 rounded-lg">{errB}</h4>}
             </div>
 
@@ -102,7 +89,7 @@ function Jacob() {
 
 
         {
-            result && <div className="flex flex-col justify-center items-center">
+            (result !== undefined || result !== null) && <div className="flex flex-col justify-center items-center">
 
                 run time: {roundNum(runTime)}ms
                 {/* result */}
@@ -110,10 +97,27 @@ function Jacob() {
                 <div ref={resultRef} className="p-2">
                     <h1 className="mb-3 border-b-2">End result:</h1>
                     <div className="flex justify-center ">
-                        <Show1DMatrix label="" matrix={result} />
+                        {result}
                     </div>
                 </div>
             </div >
+        }
+
+        {
+            oldResults && <div className="flex flex-col justify-center items-center">
+
+                {/* old results */}
+                < div className="p-2">
+                    <h1 className="mb-3 border-b-2">Previews results:</h1>
+                    <div className="flex flex-col justify-center flex-wrap ">
+                        {
+                            oldResults.map((e, i) =>
+                                <div key={i}>{e}</div>
+                            )
+                        }
+                    </div>
+                </div>
+            </div>
         }
     </div >
 }
--- pages/submissions/calculate-area-of-circle-without-pi/script.tsx
+++ pages/submissions/calculate-area-of-circle-without-pi/script.tsx
@@ -1,187 +1,45 @@
 // [  [1,1,1],  [0,2,5], [2,5,-1]]
 // [ 6,-4,27]
 
+export async function CalculateAreaOfCircle(S: number, N: number) {
+    let s = S, n = N;
 
+    const allResults: number[] = [];
+    let result = 0;
 
-export async function verifySolvable(A: number[][]) {
-    const n = A.length;
-    for (let i = 0; i < n; i++) {
-        let sum = 0;
-        for (let j = 0; j < n; j++) {
-            if (i != j) sum += Math.abs(A[i][j])
-        }
-
-        if (Math.abs(A[i][i]) <= sum) return Promise.reject("Not solvable")
-    }
-    return true;
-}
-
-
-export async function jacobMethodPrecision(_A: number[][], _B: number[], precision: number = 0.001) {
-    const A = JSON.parse(JSON.stringify(_A));
-    const B = JSON.parse(JSON.stringify(_B));
-    let x: number[] = Array(A.length).fill(0);
-    let allResults: number[][] = [];
-
-    while (true) {
-        for (let i = 0; i < A.length; i++) {
-
-            let alpha = 0;
-
-            // summing up the xs of the equation
-            for (let j = 0; j < A.length; j++) {
-                if (i != j) {
-                    alpha += A[i][j] * x[j];
-                }
-            }
-
-            // solving for the x
-            x[i] = (B[i] - alpha) / A[i][i];
-            // cb(x, _);
-            allResults.push(x.slice());
-        }
-
-        // check the amount of difference between the current and previous x
-        const diff = x.reduce((acc, curr, i) => {
-            return acc + Math.abs(curr - allResults[allResults.length - 2][i]);
-        }, 0) / x.length;
-
-        if (diff < precision) {
-            break;
-        }
-
+    while (s > 1e-10) {
+        s = Math.sqrt(1 - Math.sqrt(1 - (s * s))) / 2;
+        n *= 2;
+        const A = (n / 2) * s;
+        allResults.push(A);
+        result = A;
     }
 
-    return { x, allResults };
+    return { result, allResults }
 }
 
 
 
-
-
-export async function jacobMethodIterations(_A: number[][], _B: number[], Iterations: number) {
-    const A = JSON.parse(JSON.stringify(_A));
-    const B = JSON.parse(JSON.stringify(_B));
-    let x: number[] = Array(A.length).fill(0);
-    let allResults: number[][] = [];
-    for (let _ = 0; _ < Iterations; _++) {
-        // const previusX = x.slice();
-        for (let i = 0; i < A.length; i++) {
-
-            let alpha = 0;
-
-            // summing up the xs of the equation
-            for (let j = 0; j < A.length; j++) {
-                if (i != j) {
-                    alpha += A[i][j] * x[j];
-                }
-            }
-
-            // solving for the x
-            x[i] = (B[i] - alpha) / A[i][i];
-            // cb(x, _);
-            allResults.push(x.slice());
-        }
-    }
-
-    return { x, allResults };
-}
-
-// console.log(jaconMethodItterations([
-//     [1, 1, 1],
-//     [0, 2, 5],
-//     [2, 5, -1]
-// ], [6, -4, 27], 100, (probs, i) => {
-//     console.log(i, probs);
-// }
-// ));
 
 
 export default function viewer() {
     return <div>
         {`
-export async function verifySolvable(A: number[][]) {
-    const n = A.length;
-    for (let i = 0; i < n; i++) {
-        let sum = 0;
-        for (let j = 0; j < n; j++) {
-            if (i != j) sum += Math.abs(A[i][j])
-        }
+function CalculateAreaOfCircle(S: number, N: number) {
+    let s = S, n = N;
 
-        if (Math.abs(A[i][i]) <= sum) return Promise.reject("Not solvable")
-    }
-    return true;
-}
+    const allResults: number[] = [];
+    let result = 0;
 
-
-export async function jacobMethodPrecision(_A: number[][], _B: number[], precision: number = 0.001) {
-    const A = JSON.parse(JSON.stringify(_A));
-    const B = JSON.parse(JSON.stringify(_B));
-    let x: number[] = Array(A.length).fill(0);
-    let allResults: number[][] = [];
-
-    while (true) {
-        for (let i = 0; i < A.length; i++) {
-
-            let alpha = 0;
-
-            // summing up the xs of the equation
-            for (let j = 0; j < A.length; j++) {
-                if (i != j) {
-                    alpha += A[i][j] * x[j];
-                }
-            }
-
-            // solving for the x
-            x[i] = (B[i] - alpha) / A[i][i];
-            // cb(x, _);
-            allResults.push(x.slice());
-        }
-
-        // check the amount of difference between the current and previous x
-        const diff = x.reduce((acc, curr, i) => {
-            return acc + Math.abs(curr - allResults[allResults.length - 2][i]);
-        }, 0) / x.length;
-
-        if (diff < precision) {
-            break;
-        }
-
+    while (s > 1e-10) {
+        s = Math.sqrt(1 - Math.sqrt(1 - (s * s))) / 2;
+        n *= 2;
+        const A = (n / 2) * s;
+        allResults.push(A);
+        result = A;
     }
 
-    return { x, allResults };
-}
-
-
-
-
-
-export async function jacobMethodIterations(_A: number[][], _B: number[], Iterations: number) {
-    const A = JSON.parse(JSON.stringify(_A));
-    const B = JSON.parse(JSON.stringify(_B));
-    let x: number[] = Array(A.length).fill(0);
-    let allResults: number[][] = [];
-    for (let _ = 0; _ < Iterations; _++) {
-        // const previusX = x.slice();
-        for (let i = 0; i < A.length; i++) {
-
-            let alpha = 0;
-
-            // summing up the xs of the equation
-            for (let j = 0; j < A.length; j++) {
-                if (i != j) {
-                    alpha += A[i][j] * x[j];
-                }
-            }
-
-            // solving for the x
-            x[i] = (B[i] - alpha) / A[i][i];
-            // cb(x, _);
-            allResults.push(x.slice());
-        }
-    }
-
-    return { x, allResults };
+    return { result, allResults }
 }`}
     </div>
 }
