49a2713f451c1dd1f9c66b3dc0627653a92448b9
haouarihk <haithem2001@gmail.com>
2022-04-01 13:01:19 +0000

    1april

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '../components/Layout'
+import { useEffect } from 'react';
+
+const aprilFools = {
+  month: 3,
+  date: 1,
+};
+
+function isItAprilFoolDay() {
+  const now = new Date();
+  return now.getMonth() == aprilFools.month && now.getDate() == aprilFools.date;
+}
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    if (isItAprilFoolDay()) {
+      window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+    }
+  }, []);
   return <Layout> <Component {...pageProps} /> </Layout>
 }
 
