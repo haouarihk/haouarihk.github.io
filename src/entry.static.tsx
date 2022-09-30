0ffd8c99ac24d4eb79d0de6897e2db5639968370
haithem <haithem2001@gmail.com>
2022-09-30 23:01:18 +0100

    E

--- src/entry.static.tsx
+++ src/entry.static.tsx
@@ -3,7 +3,6 @@ import render from "./entry.ssr";
 import { fileURLToPath } from "url";
 import { join } from "path";
 
-// Execute Qwik City Static Site Generator
 qwikCityGenerate(render, {
   origin: "https://qwik.builder.io",
   outDir: join(fileURLToPath(import.meta.url), "..", "..", "dist"),
