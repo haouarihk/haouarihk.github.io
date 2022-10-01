491e43e33bddacde8263a8ab618718f36d633efc
haithem <haithem2001@gmail.com>
2022-10-01 10:35:55 +0100

    fixies

--- src/entry.ssr.tsx
+++ src/entry.ssr.tsx
@@ -1,7 +1,7 @@
-import type { RenderToStreamOptions } from '@builder.io/qwik/server';
-import { renderToStream } from '@builder.io/qwik-react';
-import { manifest } from '@qwik-client-manifest';
-import Root from './root';
+import { renderToStream, RenderToStreamOptions } from "@builder.io/qwik/server";
+
+import { manifest } from "@qwik-client-manifest";
+import Root from "./root";
 
 export default function (opts: RenderToStreamOptions) {
   return renderToStream(<Root />, {
