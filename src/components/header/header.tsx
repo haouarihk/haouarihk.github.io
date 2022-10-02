7290be0145e581b633cd1b6cb66c8aacc42b4bb7
haithem <haithem2001@gmail.com>
2022-10-02 10:26:51 +0100

    comon primary color

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -18,8 +18,8 @@ export const LinkItem = component$((props: { to: string; name: string }) => {
         href={props.to}
         class={
           currentPage
-            ? "block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white"
-            : "block py-2 pr-4 pl-3 text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+            ? "block py-2 pr-4 pl-3 text-white bg-primary rounded md:bg-transparent md:text-primary md:p-0 dark:text-white"
+            : "block py-2 pr-4 pl-3 text-white hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-primary md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
         }
         aria-current="page"
       >
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      darken: "",
-      black: "#040406",
+      colors: {
+        darken: "",
+        black: "#040406"
+      }
     },
+  },
+
+  daisyui: {
+    themes: [
+      {
+        light: {
+          ...require("daisyui/src/colors/themes")["[data-theme=light]"],
+          primary: "orange",
+          "primary-focus": "orange",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
 };
